8c8e9d462de950f999a9220b85c889f07100aa52
taemimizukura <t.m.october.arinto@gmail.com>
2025-03-06 15:48:12 +0900

    03_HelloWorldの開設

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+// class Main  org.exampleフォルダ内からしか見れない
+// オレンジ色は予約語　　
+public class Main {
+
+  //public アクセス修飾子　どこからでもアクセス可能
+  //メソッド↓
+  //println 改行(line)ありで出力される
+  public static void main(String[] args) {
+    System.out.println("こんにちは");
+  //void 返り値を求めない
+    System.out.println("HelloWorld!");
+    System.out.println(12345);
+
+  }
+}
