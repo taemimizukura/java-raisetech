bfcff872d0e0ba207f6f8d6857a40014f62a27d5
taemimizukura <t.m.october.arinto@gmail.com>
2025-03-06 16:01:55 +0900

    05_基本型_標準API_分岐処理

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,25 @@ package org.example;
 
 public class Main {
 
-    public static void main(String[] args) {
-      //文字列結合と数値計算結果の結合をして
-      // 自分の名前　＋自分の年齢　＋歳です。　が出力されるようにする。
-      int age = 1 + 3 + 1;
-      String name = "tekkun";
-      System.out.println(name + "は" + age + "歳です。");
+  public static void main(String[] args) {
+    // Trueになるパターン
+    String firstName1 = "妙美";
+    String lastName1 = "水倉";
+
+    if (lastName1.length() >= 2 && firstName1.length() >= 2) {
+      System.out.println("Trueパターン: " + firstName1);
+    } else {
+      System.out.println("Trueパターン: 条件に一致しませんでした。");
+    }
+
+    // Falseになるパターン
+    String firstName2 = "妙";
+    String lastName2 = "水倉";
+
+    if (lastName2.length() >= 2 && firstName2.length() >= 2) {
+      System.out.println("Falseパターン: " + firstName2);
+    } else {
+      System.out.println("Falseパターン: 条件に一致しませんでした。");
     }
   }
+}
