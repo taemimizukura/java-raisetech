b25d5a5a05dab7eb68571662dfc72b1ac728ca36
taemimizukura <t.m.october.arinto@gmail.com>
2025-03-07 17:50:09 +0900

    08_入出力処理と例外処理

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
+## 08_入出力処理と例外処理.
 ### ソースコード
 [Main.java](src/main/java/org/example/Main.java)
 ### 実行結果
-![実行結果](images/kadai07.png)
+![実行結果](images/kadai08.png)
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,34 @@
 package org.example;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
 
-  public static void main(String[] args) {
-    //文字列のリスト
-    List<String> fruitsList = List.of("りんご","桃","バナナ","もも","洋梨", "梨", "メロン","すいか","柿","レモン");
-    System.out.println("元のリスト" + fruitsList);
-    System.out.println("2文字以上を抽出 " + fruitsList.stream()
-        .filter(fruit -> fruit.length() >= 2)
-        .collect(Collectors.joining(",")));
+  //ファイルを作成、書き込む
+  // public static void main(String[] args) throws IOException {
+//    Path path = Path.of("JavaCourse.txt");
+//      Files.writeString(path, "Javaコース楽しい！！");
+//  }
+//}
 
-    // 数値のリスト
-    List<Integer> numberList = List.of(1, 2, 3, 4, 5, 7, 6, 7, 8, 9, 10);
+//入出力処理を実際に実装して、作ったファイルに文章を追加する。
+//  public static void main(String[] args) throws IOException {
+//    Path path = Path.of("JavaCourse.txt");
+//    Files.writeString(path,"Javaコース楽しすぎる！！", StandardOpenOption.APPEND);
+//  }
+//  }
 
-    //奇数のものだけを抽出して、平均値を出す
-
-    System.out.println(numberList.stream()
-        .filter(num -> num % 2 == 1)
-        .mapToInt(Integer::intValue)
-        .average());
+  // その作ったファイルを削除してもう一度実行すると例外になるので、その例外をキャッチして例外処理を行う
+  public static void main(String[] args) throws IOException {
+      Path path = Path.of("JavaCourse.txt");
+    try{
+      Files.writeString(path,"Javaコース楽しすぎる！！", StandardOpenOption.APPEND);
+    } catch (IOException e) {
+      System.out.println("例外が発生!");
+    }
   }
 }
+
